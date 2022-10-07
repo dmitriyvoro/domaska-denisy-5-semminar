7ad0388e2940b9be1702053f6d5556c80e924c3a
Димка я <  @mail.ru>
2022-10-07 20:14:10 +0300

    Task36 summa elementov pod nechetnini chislami

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[]array)
+{
+    for (int i=0;i<array.Length;i++)
+    array[i]=new Random().Next(100,1000);
+}
+int ReleaseArray(int []array)
+{
+
+  int sum = 0;
+  for (int i=1;i<array.Length;i+=2) 
+   
+    sum=sum+array[i];
+    
+   
+   return sum;
+}
+
+Console.Write("задайте массив:   ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[n];
+InputArray(array);
+Console.WriteLine("["+ String.Join(",", array)+ "]");
+Console.WriteLine(ReleaseArray(array));
